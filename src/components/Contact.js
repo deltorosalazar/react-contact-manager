bb0963a261b596e3d0ea2e178d8e8951fc59439b
Manuel F. Del Toro <deltorosalazar@gmail.com>
2018-08-26 22:00:24 -0500

    Context API used and now it is possible to delete contacts

---
+++ src/components/Contact.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Consumer } from '../context';
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  // In this case, we used bind()
+  onShowClick() {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  }
+
+  // In order to avoid to use bind(), use an arrow function
+  onDeleteClick = (id, dispatch) => {
+    dispatch({
+      type: 'DELETE_CONTACT',
+      payload: id
+    });
+  };
+
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}
+                <i
+                  className="fas fa-sort-down"
+                  onClick={this.onShowClick.bind(this)}
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">{email}</li>
+                  <li className="list-group-item">{phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;
